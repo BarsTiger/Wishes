dee7f6613093ce0de967bc21df3ba6e7b6e5d9b9
BarsTiger <60313248+BarsTiger@users.noreply.github.com>
2020-06-30 16:21:53 +0300

    Add files via upload

---
+++ wish-dashboard/js/data.js
@@ -0,0 +1,55 @@
+const user = {
+  name: "Андрій Мешко BarsTiger",
+  surname: "     Channel: Meowcat pro *",
+  userPhoto: "my-photo.jpg",
+  qrQode: "qr-code.svg",
+  dreams: [
+    { src: "car.jpg", alt: "Кота" },
+    { src: "home.jpg", alt: "Свій сервер" },
+    {
+      src: "hacker.webp",
+      alt: "Програмувати",
+    },
+    { src: "voyage.jpg", alt: "Багато крутих гаджетів" },
+    {
+      src: "youtube.svg.webp",
+      alt:
+        "Розвинути свій канал(щоб перейти на нього, клiкніть на лого каналу)",
+    },
+  ],
+};
+
+// arr це середня місячна платня по рокам
+const arr = [
+  500,
+  700,
+  800,
+  1900,
+  1200,
+  3000,
+  1400,
+  2900,
+  2700,
+  2800,
+  4000,
+  4400,
+];
+
+const necessaryExpenses = 1000;
+
+const todo = [
+  { month: "Червень", skill: "JavaScript" },
+  { month: "Липень", skill: "Java" },
+  { month: "Серпень", skill: "Python" },
+  { month: "Вересень", skill: "Самостійність" },
+  { month: "Жовтень", skill: "Гнучкість" },
+  { month: "Листопад", skill: "Емоційний інтелект" },
+  { month: "Грудень", skill: "Розробка ігор" },
+  { month: "Січень", skill: "Блогерство" },
+  { month: "Лютий", skill: "Лідерство" },
+  { month: "Березень", skill: "GitHub" },
+  { month: "Квітень", skill: "AI" },
+  { month: "Травень", skill: "Робити ігри і сайти до них" },
+];
+
+export { user, arr, necessaryExpenses, todo };
